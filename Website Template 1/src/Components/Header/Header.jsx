b5665f1532a0f65781bd1dd512cc99401ec89a81
Steven King <kingsteven1113@gmail.com>
2024-11-24 00:11:53 -0500

    Working on Finishing responsive design for header

--- Website Template 1/src/Components/Header/Header.jsx
+++ Website Template 1/src/Components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React, {useContext} from 'react'
 import './Header.css'
+import { IoIosMenu } from "react-icons/io";
 
 const Header = () => {
     
       return (
+        <>
         <div className='Header'>
             <a href='/' className='Logo'>Dental Office</a>
           <ul className='HeaderList'>
@@ -18,8 +20,26 @@ const Header = () => {
                 <a className='MenuItemA' href="/reviews">Reviews</a>
             </li>
           </ul>
-                <a className='MenuItemB' href="/contact">Contact</a>
+            <div className='MobileContact'>
+            <a className='MenuItemB' href="/contact">Contact</a>
+            <IoIosMenu className="Burger" />
+            </div>     
         </div>
+        <div>
+        <ul className='HeaderListMobile'>
+            <li className='ListItemA'>
+                <a className='MenuItemA' href="/about">About Us</a>
+                <div className='AccentAnimation'></div>
+            </li>
+            <li className='ListItemA'>
+                <a className='MenuItemA' href="/services">Services</a>
+            </li>
+            <li className='ListItemA'>
+                <a className='MenuItemA' href="/reviews">Reviews</a>
+            </li>
+          </ul>
+        </div>
+        </>
       );
 
 }
