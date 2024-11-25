29fea3681e916762ca92e5d97fdb8057dafcf99c
kingsteven1113 <125849798+kingsteven1113@users.noreply.github.com>
2024-11-25 10:00:30 -0500

    Working on HTML for Landing Page

--- Website Template 1/src/Components/LandingPage/LandingPage.jsx
+++ Website Template 1/src/Components/LandingPage/LandingPage.jsx
@@ -14,6 +14,50 @@ const LandingPage = () => {
             </div>
             <video className='HeroImage' loop autoPlay muted src={HeroVideo}></video>
             </div>
+
+            <div className='MeetTheTeam'>
+              <h2 className='TeamH2'>Meet Our Team</h2>
+              <div className='TeamMember'>
+                Name
+                <img src="" alt="" />
+                Team Member
+              </div>
+              <div className='TeamMember'>
+                Name
+                <img src="" alt="" />
+                Team Member
+              </div>
+              <div className='TeamMember'>
+                Name
+                <img src="" alt="" />
+                Team Member
+              </div>
+              <div className='TeamMember'>
+                Name
+                <img src="" alt="" />
+                Team Member
+              </div>
+              <div className='WhoWeAre'>
+                <img src="" alt="" />
+                <h2>Who We Are</h2>
+                <p>Something about what makes Premier Dental Different from the other dental offices you see in the area.</p>
+                <button>Reviews</button>
+              </div>
+              <div className='ContactForm'>
+                  <form action="">
+                    <h3>Name</h3>
+                    <input type="text" />
+                    <h3>Reason For Your Visit</h3>
+                    <input type="text" />
+                    <h3>Email</h3>
+                    <input type="email" name="" id="" />
+                    <h3>Phone Number</h3>
+                    <input type="tel" name="" id="" />
+                    <h3>Last Dental Visit</h3>
+                    <input type="date" name="" id="" />
+                  </form>
+              </div>
+            </div>
         </div>
       );
 
