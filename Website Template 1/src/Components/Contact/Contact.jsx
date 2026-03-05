6afd4295019d9cb18791c4f1d2575e1e8d082f40
Steven King <kingsteven1113@gmail.com>
2026-03-05 17:52:41 -0500

    slight update to the modal

--- Website Template 1/src/Components/Contact/Contact.jsx
+++ Website Template 1/src/Components/Contact/Contact.jsx
@@ -1,11 +1,21 @@
 import React, {useContext} from 'react'
+import ContactImage from '../../assets/DentalContactHeroImage.jpg'
 import './Contact.css'
 
 const Contact = () => {
     
       return (
         <div>
-          CONTACT
+          <div className='ContactHero'>
+            <img src={ContactImage} alt="Contact" />
+            <form method="POST" action='' className='ContactForm'>
+              <h1>Contact Us</h1>
+              <input type="text" name="name" placeholder='Name' required />
+              <input type="email" name="email" placeholder='Email' required />
+              <textarea name="message" placeholder='Message' required></textarea>
+              <button type="submit">Send Message</button>
+              </form>
+          </div>
         </div>
       );
 
